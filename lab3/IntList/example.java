c95b0289cbc35b08769deb38b79c716aff942e31
Haofei Ma <mahaofei001@gmail.com>
2022-04-28 20:42:50 -0400

    all solution

--- lab3/IntList/example.java
+++ lab3/IntList/example.java
@@ -1,2 +1,18 @@
+import java.lang.*;
 public class example {
+    public static int[] flatten(int[][] x) {
+        int totalLength = 0;
+        for (int i = 0; i < x.length; i++) {
+            totalLength += x[i].length;
+        }
+
+        int[] a = new int[totalLength];
+        int aIndex = 0;
+        for (int i = 0; i < x.length; i++) {
+            System.arraycopy(x[i], 0, a, aIndex, x[i].length);
+            aIndex += x[i].length;
+
+        }
+        return a;
+    }
 }
--- lab3/IntList/exampleTest.java
+++ lab3/IntList/exampleTest.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class exampleTest {
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class exampleTest {
+    @Test
+    public void test(){
+        int[][] a = {{1, 2, 3}, {5,6}, {7, 8}};
+        int [] x = {1,2,3,5,6,7,8};
+        assertArrayEquals(x, example.flatten(a));
+    }
+
 }
